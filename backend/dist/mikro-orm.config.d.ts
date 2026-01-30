b3cca12b76ab63e127e1ce2c4d478728e6c45a6a
thanush96 <thanushkanth@linearsix.com>
2026-01-30 18:38:13 +0530

    chore(backend): remove dist and node_modules from git tracking

--- backend/dist/mikro-orm.config.d.ts
+++
@@ -1,3 +0,0 @@
-import { PostgreSqlDriver } from '@mikro-orm/postgresql';
-declare const _default: import("@mikro-orm/core").Options<PostgreSqlDriver, import("@mikro-orm/postgresql").EntityManager<PostgreSqlDriver> & import("@mikro-orm/core").EntityManager<import("@mikro-orm/postgresql").IDatabaseDriver<import("@mikro-orm/postgresql").Connection>>>;
-export default _default;
